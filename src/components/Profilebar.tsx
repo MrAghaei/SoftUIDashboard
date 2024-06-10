864b61541c6b606afd3d704b7c4a0e48076e7c10
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-06-10 20:30:54 +0330

    [#6-UPDATE] project page

--- src/components/Profilebar.tsx
+++ src/components/Profilebar.tsx
@@ -2,8 +2,9 @@
 
 function Profilebar(){
     return(
-        <div>
-            <div className='size-72 bg-gradient-to-r from-primary-light to-primary-dark'>dqdfafdsafasdfafws</div>
+        <div className='w-screen mt-10 mr-40 ml-96 flex flex-col size relative'>
+            <div className='h-56 w-auto rounded-3xl bg-gradient-to-r from-primary-light to-primary-dark'></div>
+            <div className='w-1/2 h-32 bg-white rounded-3xl shadow-2xl absolute translate-x-1/2 translate-y-40'></div>
         </div>
 
 
