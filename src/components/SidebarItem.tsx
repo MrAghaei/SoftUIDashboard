e0ec449352d20e67efbbd8d12b51358367feb35d
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-06-13 10:10:45 +0330

    [#7-REFACTOR] sidebar component (#12)
    * [#7-REFACTOR] sidebar component
    
    turn it into three components
    
    * [#7-REFACTOR] sidebaritem
    
    add map method to reduce code

--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -1,10 +1,13 @@
 // src/components/SidebarItem.jsx (or .tsx)
 import React from 'react';
 
-const SidebarItem = ({ title }) => (
-    <li className="p-2 hover:bg-gray-100 transition-colors duration-300 ease-in-out">
-        {title}
-    </li>
+const SidebarItem = ({items} ) => (
+    items.map(item=> (
+        <li className="p-2 hover:bg-gray-100 transition-colors duration-300 ease-in-out">
+            {item}
+        </li>
+    ))
+
 );
 
 export default SidebarItem;
