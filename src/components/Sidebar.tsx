8649f6c26a5f999ce760e6c0cbe4a6c49dadb3fb
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-06-15 20:32:41 +0330

    [#7-REFACTOR] refactor sidebar component (#13)
    * [#7-REFACTOR] sidebar component
    
    turn it into three components
    
    * [#7-REFACTOR] sidebaritem
    
    add map method to reduce code
    
    * [#7-UPDATE] sidebar & sidebar section
    
    refactor interface and passing data to children

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,3 @@
-// src/components/Sidebar.jsx (or .tsx)
 import React from 'react';
 import { FaHome, FaUser, FaShoppingCart, FaSignInAlt } from 'react-icons/fa';
 import SidebarSection from './SidebarSection';
@@ -10,6 +9,8 @@ const Sidebar = () => {
     const ApplicationItems: string[] = ['Kanban','Wizard', 'DataTables', 'Calendar', 'Analytics'];
     const EcommerceItems: string[] = ['Overview','Products', 'Orders', 'Referral'];
     const AuthenticationItems: string[] = ['Sign In','Sign Up', 'Reset Password', 'Lock', '2-step Verification', 'Error'];
+    const PagesClassNames = "bg-white shadow-2xl rounded-2xl"
+
     return (
         <div className="fixed bg-gray-100 p-8 w-72 h-full shadow-md text-secondary-text overflow-y-scroll z-30">
             <div className="p-4 flex items-center space-x-3">
@@ -18,27 +19,57 @@ const Sidebar = () => {
             </div>
             <nav className="mt-4">
                 <ul>
-                    <SidebarSection icon={FaUser} title="Dashboards">
-                        <SidebarItem items={DashboardItems} />
-                    </SidebarSection>
+                    <SidebarSection
+                        icon={FaUser}
+                        data={{
+                            title: 'Dashboards',
+                            isInitiallyOpen: false,
+                            className: '',
+                            children: <SidebarItem items={DashboardItems} />
+                        }}
+                    />
 
                     <p className='font-bold pb-1 pt-5'>PAGES</p>
 
-                    <SidebarSection icon={FaUser} title="Pages">
-                        <SidebarItem items={PagesItems} />
-                    </SidebarSection>
+                    <SidebarSection
+                        icon={FaUser}
+                        data={{
+                            title: 'Pages',
+                            isInitiallyOpen: false,
+                            className: PagesClassNames,
+                            children: <SidebarItem items={PagesItems} />
+                        }}
+                    />
 
-                    <SidebarSection icon={FaUser} title="Applications">
-                        <SidebarItem items={ApplicationItems} />
-                    </SidebarSection>
+                    <SidebarSection
+                        icon={FaUser}
+                        data={{
+                            title: 'Applications',
+                            isInitiallyOpen: false,
+                            className: '',
+                            children: <SidebarItem items={ApplicationItems} />
+                        }}
+                    />
 
-                    <SidebarSection icon={FaShoppingCart} title="Ecommerce">
-                        <SidebarItem items={EcommerceItems} />
-                    </SidebarSection>
+                    <SidebarSection
+                        icon={FaShoppingCart}
+                        data={{
+                            title: 'Ecommerce',
+                            isInitiallyOpen: false,
+                            className: '',
+                            children: <SidebarItem items={EcommerceItems} />
+                        }}
+                    />
 
-                    <SidebarSection icon={FaSignInAlt} title="Authentication">
-                        <SidebarItem items={AuthenticationItems} />
-                    </SidebarSection>
+                    <SidebarSection
+                        icon={FaSignInAlt}
+                        data={{
+                            title: 'Authentication',
+                            isInitiallyOpen: false,
+                            className: '',
+                            children: <SidebarItem items={AuthenticationItems} />
+                        }}
+                    />
                 </ul>
             </nav>
         </div>
--- src/components/SidebarSection.tsx
+++ src/components/SidebarSection.tsx
@@ -1,24 +1,35 @@
-// src/components/SidebarSection.jsx (or .tsx)
-import React, { useState } from 'react';
+import React, { useState, ReactNode, FC } from 'react';
 import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
 
-const SidebarSection = ({ icon: Icon, title, children, isInitiallyOpen = false, }) => {
-    const [isOpen, setIsOpen] = useState(isInitiallyOpen);
+interface SidebarSectionInputDataModel {
+    className?: string;
+    title: string;
+    children: ReactNode;
+    isInitiallyOpen: boolean;
+}
+
+interface SidebarSectionInputModel {
+    data: SidebarSectionInputDataModel;
+    icon: React.ElementType;
+}
+
+const SidebarSection: FC<SidebarSectionInputModel> = ({ data, icon: Icon }) => {
+    const [isOpen, setIsOpen] = useState(data.isInitiallyOpen);
 
     return (
         <>
             <li
-                className="p-4 cursor-pointer flex items-center justify-between bg-white shadow-2xl rounded-2xl transition-colors duration-300 ease-in-out"
-                onClick={() => { setIsOpen(!isOpen)}}
+                className={`p-4 cursor-pointer flex items-center justify-between transition-colors duration-300 ease-in-out ${data.className}`}
+                onClick={() => setIsOpen(!isOpen)}
             >
                 <div className="flex items-center space-x-2">
                     <Icon />
-                    <span>{title}</span>
+                    <span>{data.title}</span>
                 </div>
                 {isOpen ? <FiChevronUp /> : <FiChevronDown />}
             </li>
             <ul className={`pl-8 overflow-hidden transition-max-height duration-300 ease-in-out ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
-                {children}
+                {data.children}
             </ul>
         </>
     );
