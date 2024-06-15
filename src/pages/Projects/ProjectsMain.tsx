bbdbf3c09e522aa59d8d40907ad005de78d4d975
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-06-15 21:23:28 +0330

    [#8] project page (#14)
    * [#8-UPDATE] project page
    
    * [#8-ADD] ProjectCard component
    
    * [#8-UPDATE] ProjectCard

--- src/pages/Projects/ProjectsMain.tsx
+++ src/pages/Projects/ProjectsMain.tsx
@@ -1,12 +1,26 @@
 import Sidebar from "../../components/Sidebar.tsx";
 import Profilebar from "../../components/Profilebar.tsx";
+import ProjectCard from "./ProjectCard.tsx";
 
 function ProjectsMain(){
     return(
-        <div className='flex flex-row'>
+        <div className='grid'>
             <Sidebar/>
             <Profilebar/>
+            <div className='flex flex-col gap-2 items-start pl-96'>
+                <p className='text-2xl text-primary-text'>Some of Our Awesome Projects</p>
+                <p className='text-secondary-text'>This is the paragraph where you can write more details about your projects. Keep you user engaged by providing meaningful information.</p>
+            </div>
+            <div className='grid gap-5 grid-cols-3 grid-rows-2 pl-96 mt-10 mb-10'>
+                <ProjectCard/>
+                <ProjectCard/>
+                <ProjectCard/>
+                <ProjectCard/>
+                <ProjectCard/>
+                <ProjectCard/>
+            </div>
         </div>
+
 
 
     )
